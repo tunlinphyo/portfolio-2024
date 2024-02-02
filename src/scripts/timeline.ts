217cb240d3e9844f8046e5709a8a3093b219dce2
tunlinphyo <uniqueof1990@gmail.com>
2024-02-02 10:34:34 +0900

    update

--- src/scripts/timeline.ts
+++ src/scripts/timeline.ts
@@ -1,21 +1,19 @@
 import { SCRUB } from "./helpers/const"
 import gsap from "./helpers/gsap"
-import { debounce, elem } from "./helpers/utils"
+import { elem } from "./helpers/utils"
 
 export default class Timeline {
     readonly timeline: GSAPTimeline
-    media: gsap.MatchMedia
+    readonly media: gsap.MatchMedia
 
     constructor() {
         gsap.scrollTrigger.config({
             // limitCallbacks: true,
             autoRefreshEvents: "resize",
-            // syncInterval: 999999999,
             ignoreMobileResize: true,
         })
 
         gsap.scrollTrigger.saveStyles(".header-name")
-        // ScrollTrigger.normalizeScroll(true)
 
         this.timeline = gsap.createTimeline({
             trigger: elem(".scroll-trigger"),
@@ -25,18 +23,5 @@ export default class Timeline {
         })
 
         this.media = gsap.matchMedia()
-
-        this.subscribe()
-    }
-
-    private subscribe() {
-        const debounceRefresh = debounce(() => {
-            this.media = gsap.matchMedia()
-            this.timeline.scrollTrigger?.refresh()
-        }, 500)
-
-        gsap.scrollTrigger.addEventListener("refreshInit", () => {
-            debounceRefresh()
-        })
     }
 }
